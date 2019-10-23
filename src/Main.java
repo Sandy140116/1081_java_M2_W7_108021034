256840a1b1d1ac98242b5f2e0eead79146dcbb28
Sandy <c09050315828@gmail.com>
2019-10-23 23:28:12 +0800

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int m = n/2;
+        boolean flog = true;
 
-        int sum = 0, ct;
-        boolean flag = true;
-        for (int i = 2; i <= n; i++) {
-            ct = i;
-            for (; ct != 1; ) {
-                if (ct % 2 == 0) {
-                    ct /= 2;
-                } else if (ct % 3 == 0) {
-                    ct /= 3;
-                } else if (ct % 5 == 0) {
-                    ct /= 5;
-                } else {
-                    flag = false;
-                    break;
-                }
+        for (int i=2;i<m;i++){
+            if (n%i == 0){
+                flog = false;
+                break;
             }
-            if (flag) sum += i;
-            flag = true;
         }
-        System.out.println(sum);
+        if (flog){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
     }
 }
 
