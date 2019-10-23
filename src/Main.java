5313702906d97cfb4ab9ff82a9a8e9052caebed0
Sandy <c09050315828@gmail.com>
2019-10-23 23:24:43 +0800

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,31 @@
 
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
 
-        int sum = 0,i=1,j=1;
-        for (i=1;i<=n;i++){
-            for (j=1;j<n;j++){
-                sum = i*j;
-                System.out.print(sum+"\t");
+        int sum = 0, ct;
+        boolean flag = true;
+        for (int i = 2; i <= n; i++) {
+            ct = i;
+            for (; ct != 1; ) {
+                if (ct % 2 == 0) {
+                    ct /= 2;
+                } else if (ct % 3 == 0) {
+                    ct /= 3;
+                } else if (ct % 5 == 0) {
+                    ct /= 5;
+                } else {
+                    flag = false;
+                    break;
+                }
             }
-            System.out.println(i*(j));
+            if (flag) sum += i;
+            flag = true;
         }
-    }
-}
-
+        System.out.println(sum);
     }
 }
 
